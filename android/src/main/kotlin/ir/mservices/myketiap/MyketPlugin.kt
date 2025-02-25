9a6bd3df828a84226ef98e83551183a21eb7f460
Javad Rahmati <vovin.developer@gmail.com>
2025-02-25 11:01:15 +0330

    Remove deprecated registerWith from plugin

--- android/src/main/kotlin/ir/mservices/myketiap/MyketPlugin.kt
+++ android/src/main/kotlin/ir/mservices/myketiap/MyketPlugin.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import ir.myket.billingclient.IabHelper
 import ir.myket.billingclient.util.IabResult
 import ir.myket.billingclient.util.Inventory
@@ -33,25 +32,6 @@ class MyketPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "myket")
         channel.setMethodCallHandler(this)
         context = flutterPluginBinding.applicationContext
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "myket")
-            val myketIapPlugin = MyketPlugin()
-            channel.setMethodCallHandler(myketIapPlugin)
-            myketIapPlugin.context = registrar.activeContext()
-        }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
